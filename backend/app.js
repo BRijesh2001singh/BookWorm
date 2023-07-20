a4d744ebefb22b5f484216ff65b656e1b2fbccc7
Brijesh <brijesh1205singh@gmail.com>
2023-07-20 14:31:31 +0530

    third commit

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,11 @@ const app = express();
 const bookroute = require("./routes/bookroutes");
 const bookmodel = require("./models/bookmodel");
 const cors = require('cors');
+const PORT = process.env.PORT || 1000;
 require("./connection/conncetion");
 app.use(cors())
 app.use(express.json());
 app.use("/api/v1/", bookroute);
-app.listen(1000, () => {
+app.listen(PORT, () => {
     console.log("CONNECTED");
 });
